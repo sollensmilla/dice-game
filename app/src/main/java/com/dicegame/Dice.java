7f49493d17925789207e8b79635611c9c19324b8
Maria Mair <mm225mz@student.lnu.se>
2025-11-12 15:05:10 +0100

    GREEN: Add Dice class

---
+++ app/src/main/java/com/dicegame/Dice.java
@@ -0,0 +1,13 @@
+package com.dicegame;
+
+import java.util.Random;
+
+public class Dice {
+  private Random random = new Random();
+  private int faceValue;
+
+  public int roll() {
+    this.faceValue = random.nextInt(6) + 1;
+      return this.faceValue;
+  }
+}
--- app/src/test/java/com/dicegame/DiceTest.java
+++ app/src/test/java/com/dicegame/DiceTest.java
@@ -1,8 +1,6 @@
 package com.dicegame;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 public class DiceTest {
