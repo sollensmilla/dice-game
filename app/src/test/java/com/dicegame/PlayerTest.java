9a8134548a8e393a4856fde9ef1216fd1438279c
Smilla Sollén <ss226uk@student.lnu.se>
2025-11-14 11:12:27 +0100

    GREEN: Add Player class

--- app/src/test/java/com/dicegame/PlayerTest.java
+++ app/src/test/java/com/dicegame/PlayerTest.java
@@ -1,6 +1,9 @@
 package com.dicegame;
 
+import org.junit.jupiter.api.Test;
+
 public class PlayerTest {
+    @Test
     public void playerShouldHaveAName() {
         Player player = new Player("Alice");
         assert player.getName().equals("Alice") : "Player name should be Alice";
