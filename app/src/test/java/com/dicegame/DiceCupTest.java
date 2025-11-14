e21dbb3d499fb52b03934537eeaba6fde93b93d4
Smilla Sollén <ss226uk@student.lnu.se>
2025-11-14 10:54:20 +0100

    RED: Add test for dice cup summing up two dice

---
+++ app/src/test/java/com/dicegame/DiceCupTest.java
@@ -0,0 +1,22 @@
+package com.dicegame;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class DiceCupTest {
+    @Test
+    public void diceCupShouldSumUpTwoDices() {
+        Dice dice1 = Mockito.mock(Dice.class);
+        Dice dice2 = Mockito.mock(Dice.class);
+
+        Mockito.when(dice1.roll()).thenReturn(3);
+        Mockito.when(dice2.roll()).thenReturn(5);
+
+        DiceCup diceCup = new DiceCup(dice1, dice2);
+        int sum = diceCup.rollAndSum();
+
+        assertEquals(sum, 8, "Sum should be 8");
+    }
+}
