692ba5140c7da93ab6070ce0a1010785e2f90732
Smilla Sollén <ss226uk@student.lnu.se>
2025-11-18 11:12:07 +0100

    REFACTOR: Use dependency injection to make the console testable

--- app/src/test/java/com/dicegame/ConsoleTest.java
+++ app/src/test/java/com/dicegame/ConsoleTest.java
@@ -3,17 +3,29 @@ package com.dicegame;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 public class ConsoleTest {
     @Test
     public void consoleShouldPromptForPlayerNames() {
-       Console console = new Console();
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(output);
 
-       assertArrayEquals(new String[]{"Enter name for Player 1:", "Enter name for Player 2:"},
-               console.promptForPlayerNames(),
-               "Console should prompt for two player names");
+        Console console = new Console(new Scanner(""), printStream);
+
+         console.askForPlayerNames();
+
+        String result = output.toString().trim();
+
+               assertEquals(
+            "Enter name for Player 1:\nEnter name for Player 2:",
+            result
+        );
 
     }
 }
