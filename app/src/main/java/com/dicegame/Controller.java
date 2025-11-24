c088603b4923f53a7da4a810cdcda1c88315d6d0
Maria Mair <mm225mz@student.lnu.se>
2025-11-24 14:29:03 +0100

    GREEN: Add Controller class

---
+++ app/src/main/java/com/dicegame/Controller.java
@@ -0,0 +1,25 @@
+package com.dicegame;
+
+import java.util.List;
+
+public class Controller {
+  private Game game;
+  private Console console;
+
+  public Controller(Game game, Console console) {
+    this.game = game;
+    this.console = console;
+  }
+  
+/*   public void setUpGame() {
+    String[] playerNames = console.promptForPlayerNames();
+    for (String name : playerNames) {
+        Player player = new Player(name);
+        game.addPlayer(player);
+    }
+
+    List<Player> players = game.getPlayers();
+
+console.displayWelcomeScreen(players.get(0).getName(), players.get(1).getName());
+} */
+}
