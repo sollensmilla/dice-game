42b7161182e3bd8f7f02f27d9d4a595f4a7d3f87
Maria Mair <mm225mz@student.lnu.se>
2025-11-24 14:51:33 +0100

    RED: Add test for round winner message

---
+++ app/src/test/java/com/dicegame/MessageTest.java
@@ -0,0 +1,14 @@
+package com.dicegame;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class MessageTest {
+  @Test
+  public void getRoundWinnerMessageShouldContainNameForRoundWinner() {
+    Message message = new Message();
+    String roundWinnerMessage = "Alice wins this round.";
+    String result = message.getRoundWinnerMessage();
+    assertEquals(result, roundWinnerMessage);
+  }
+}
