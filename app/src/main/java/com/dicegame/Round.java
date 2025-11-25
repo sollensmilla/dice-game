b32a343bd39de6f91929cbb8d772b30272f938a7
Smilla Sollén <ss226uk@student.lnu.se>
2025-11-25 10:39:13 +0100

    GREEN: Implement Round class that plays out a round

---
+++ app/src/main/java/com/dicegame/Round.java
@@ -0,0 +1,34 @@
+package com.dicegame;
+
+public class Round {
+    private Player player1;
+    private Player player2;
+    private DiceCup diceCup;
+
+    public Round(Player player1, Player player2, DiceCup diceCup) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.diceCup = diceCup;
+    }
+
+      public Player playRound() {
+        int player1Roll = diceCup.rollAndSum();
+        int player2Roll = diceCup.rollAndSum();
+
+        Player winner = compareFaceValues(player1Roll, player2Roll);
+        if (winner != null) {
+            winner.addScore();
+        }
+        return winner;
+    }
+
+    private Player compareFaceValues(int player1Roll, int player2Roll) {
+        if (player1Roll > player2Roll) {
+            return player1;
+        } else if (player2Roll > player1Roll) {
+            return player2;
+        } else {
+            return null;
+        }
+    }
+}
