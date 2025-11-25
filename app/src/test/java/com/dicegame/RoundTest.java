d455d87f8742400d046772016a1d9a6bc826ef5e
Smilla Sollén <ss226uk@student.lnu.se>
2025-11-25 10:42:18 +0100

    REFACTOR: Move tests that belong to the playRound method to new test class for Round

--- app/src/test/java/com/dicegame/RoundTest.java
+++ app/src/test/java/com/dicegame/RoundTest.java
@@ -11,19 +11,19 @@ public class RoundTest {
     private DiceCup diceCupMock;
     private Player player1;
     private Player player2;
+    private Round round;
 
     @BeforeEach
     public void setUp() {
         diceCupMock = Mockito.mock(DiceCup.class);
-
         player1 = new Player("Alice");
         player2 = new Player("Eva");
+        
+        round = new Round(player1, player2, diceCupMock);
     }
 
     @Test
     public void playRoundShouldCheckWhichPlayerRollsHighestForEachRound() {
-         Round round = new Round(player1, player2, diceCupMock);
-         
         Mockito.when(diceCupMock.rollAndSum())
                 .thenReturn(7)
                 .thenReturn(10);
@@ -32,5 +32,28 @@ public class RoundTest {
         Mockito.verify(diceCupMock, Mockito.times(2)).rollAndSum();
 
         assertEquals(player2, winner, "Eva should win with higher roll");
+    }
+
+    @Test
+    public void playRoundShouldNotAwardPointsForPlayerTie() {
+        Mockito.when(diceCupMock.rollAndSum())
+                .thenReturn(8)
+                .thenReturn(8);
+
+        round.playRound();
+        assertEquals(0, player1.getScore(), "Alice should have 0 points after tie");
+        assertEquals(0, player2.getScore(), "Eva should have 0 points after tie");
+    }
+
+    @Test
+    public void playRoundShouldAwardPointToPlayerForWinningRound() {
+        Mockito.when(diceCupMock.rollAndSum())
+                .thenReturn(12)
+                .thenReturn(10);
+
+        round.playRound();
+
+        assertEquals(1, player1.getScore(), "Alice should have 1 point after winning");
+        assertEquals(0, player2.getScore(), "Eva should have 0 point after losing");
     }
 }
