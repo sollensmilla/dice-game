02b5dd7d4c3150e7490559b9dd2ab045749eacbf
Maria Mair <mm225mz@student.lnu.se>
2025-11-26 10:30:28 +0100

    REFACTOR: Use beforeEach to set up RoundResult object

--- app/src/test/java/com/dicegame/RoundResultTest.java
+++ app/src/test/java/com/dicegame/RoundResultTest.java
@@ -2,20 +2,25 @@ package com.dicegame;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RoundResultTest {
+  private RoundResult roundResult;
 
-  @Test
-  public void getPlayer1ShouldReturnNameForPlayer1() {
+  @BeforeEach
+  public void setUp() {
     String player1 = "Alice";
     int sum1 = 7;
     String player2 = "Eva";
     int sum2 = 10;
     String winner = "Eva";
-    
-    RoundResult roundResult = new RoundResult(player1, sum1, player2, sum2, winner);
 
+    roundResult = new RoundResult(player1, sum1, player2, sum2, winner);
+  }
+
+  @Test
+  public void getPlayer1ShouldReturnNameForPlayer1() {
     assertEquals("Alice", roundResult.getPlayer1());
   }
   
